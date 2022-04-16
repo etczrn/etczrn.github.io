552562ada86685d018af82b85e8f364f6fb01000
etczrn <etczrn@gmail.com>
2022-04-16 21:50:28 +0900

    Add typescript and test tsx file

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,5 +128,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
   ],
 }
---
+++ src/pages/test.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Test = () => {
+  return <>Is it working?</>
+}
+
+export default Test
