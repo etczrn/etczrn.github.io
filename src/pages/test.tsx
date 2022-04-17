872fc7c3a37c9c0a196a89784bc8c46e4d664c7a
etczrn <etczrn@gmail.com>
2022-04-17 19:58:03 +0900

    Move user component to components folder

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,5 +1,5 @@
-import React from "react"
 import styled from "styled-components"
+import { User } from "components"
 
 const Container = styled.div`
   margin: 3rem auto;
@@ -9,41 +9,6 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
 `
-const UserWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 0 auto 12px auto;
-  &:last-child {
-    margin-bottom: 0;
-  }
-`
-const Avatar = styled.img`
-  flex: 0 0 96px;
-  width: 96px;
-  height: 96px;
-  margin: 0;
-`
-const Description = styled.div`
-  flex: 1;
-  margin-left: 18px;
-  padding: 12px;
-`
-const Username = styled.h2`
-  margin: 0 0 12px 0;
-  padding: 0;
-`
-const Excerpt = styled.p`
-  margin: 0;
-`
-const User = props => (
-  <UserWrapper>
-    <Avatar src={props.avatar} alt="" />
-    <Description>
-      <Username>{props.username}</Username>
-      <Excerpt>{props.excerpt}</Excerpt>
-    </Description>
-  </UserWrapper>
-)
 
 const UsersList = () => {
   return (
