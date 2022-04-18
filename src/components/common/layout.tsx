b0e52be64c22d690f6e377cbe37c46bdc359ace9
etczrn <etczrn@gmail.com>
2022-04-18 21:29:19 +0900

    Create common layout component

---
+++ src/components/common/layout.tsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components"
+
+// Josh's Custom CSS Reset
+// https://www.joshwcomeau.com/css/custom-css-reset/
+const GlobalStyle = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+}
+html, body {
+  height: 100%;
+}
+body {
+  font-family: "Inter",-apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+input, button, textarea, select {
+  font: inherit;
+}
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+#root, #__next {
+  isolation: isolate;
+}
+`
+
+export const Layout: React.FC = ({ children }) => {
+  return <GlobalStyle>{children}</GlobalStyle>
+}
